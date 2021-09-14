3eb7be93832f802e3f446e9e81aeba4664554de1
Muflah Nasir <muflahnasir@gmail.com>
2021-09-14 12:13:05 +0500

    Created get posts query

--- server/index.ts
+++ server/index.ts
@@ -2,18 +2,31 @@ const { ApolloServer } = require('apollo-server')
 const gql = require("graphql-tag")
 const mongoose = require("mongoose")
 
-const Post = require("./models/Post ")
+const Post = require("./models/Post.tsx")
 const {MONGODB} = require("./config.ts")
 
 const typeDefs = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+    }
     type Query{
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 
 const resolvers = {
     Query: {
-        sayHi: () => "Hello World"
+        async getPosts(){
+            try {
+                const posts = await Post.find() 
+                return posts
+            } catch (error) {
+                throw new Error(error)
+            }
+        }
     }
 } 
 
