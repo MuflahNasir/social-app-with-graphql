2171849e10565be467f6f83c54c9f68cba0054cd
Muflah Nasir <muflahnasir@gmail.com>
2021-09-16 13:23:24 +0500

    validate data for resgistration and login

--- server/graphql/resolvers/users.tsx
+++ server/graphql/resolvers/users.tsx
@@ -1,7 +1,8 @@
-const bycrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const {UserInputError} = require("apollo-server")
 
+const {validateRegisterInput, validateLoginInput} = require("../../utils/validators")
 const User = require("../../models/User.tsx")
 const {SECRET_KEY} = require("../../config")
 
@@ -15,10 +16,73 @@ type IRegisterInput = {
     confirmPassword: String
 }
 
+type ILoginInput = {
+    email:  String,
+    password: String
+}
+
+type IUser = {
+    id: String,
+    username: String,
+    email: String
+}
+
+function generateToken(user: any){
+    console.log(user.username)
+    jwt.sign({
+        id: user.id,
+        username: user.username,
+        email: user.email
+    }, SECRET_KEY, {expiresIn: '1h'})
+}
+
 module.exports = {
     Mutation: {
+        async login(_: any, {email, password}: ILoginInput) {
+            //valid login data
+            const {valid, errors} = validateLoginInput(email, password)
+            if(!valid){
+                throw new UserInputError('Errors', {errors})
+            }
+
+            //check if user exist
+             const user = await User.findOne({email})
+            if(!user){
+                throw new UserInputError("User doesn't exist", {
+                    errors: {
+                        email: 'User is not registered'
+                    }
+                })
+            }
+
+            //check if password is correct
+            const match = await bcrypt.compare(password, user.password)
+            if(!match){
+                throw new UserInputError("Wrong credentials", {
+                    errors: {
+                        email: 'Wrong credentials'
+                    }
+                })
+            }
+
+            const token = generateToken(user)
+            console.log(token)
+
+            return {
+                ...user._doc,
+                id: user._id,
+                token
+            }
+        },
         async register(_: any, {registerInput: {username, email, password, confirmPassword}}: IRegister){
             
+            //valid user data
+            const {valid, errors} = validateRegisterInput(username, email, password, confirmPassword)
+            if(!valid){
+                throw new UserInputError('Errors', {errors})
+            }
+
+            //Make sure user doesn't exist
             const user = await User.findOne({email})
             if(user){
                 throw new UserInputError('Email already exist', {
@@ -27,7 +91,9 @@ module.exports = {
                     }
                 })
             }
-            password = await bycrypt.hash(password, 12)
+
+            //Hash password
+            password = await bcrypt.hash(password, 12)
 
             const newUser = new User({
                 email,
@@ -38,11 +104,7 @@ module.exports = {
 
             const result = await newUser.save()
 
-            const token = jwt.sign({
-                id: result.id,
-                username: result.username,
-                email: result.email
-            }, SECRET_KEY, {expiresIn: '1h'})
+            const token = generateToken(result)
 
             return {
                 ...result._doc,
