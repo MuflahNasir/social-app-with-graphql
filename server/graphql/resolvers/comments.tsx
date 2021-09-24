05cc0399ae13c2d4895dddbdaf80f123bdeec762
Muflah Nasir <muflahnasir@gmail.com>
2021-09-24 16:53:23 +0500

    Created Delete comment mutation

--- server/graphql/resolvers/comments.tsx
+++ server/graphql/resolvers/comments.tsx
@@ -8,6 +8,18 @@ type ICreatePost = {
     body: String
 }
 
+type IDeleteCom = {
+    postId: String,
+    commentId: String
+}
+
+type Comments = {
+    id: String,
+    body: String,
+    username: String,
+    createdAt: String
+}
+
 module.exports = {
     Mutation: {
         createComment: async(_: any, {postId, body}: ICreatePost, context: any) => {
@@ -29,7 +41,25 @@ module.exports = {
                     })
                     await post.save()
                     return post
-                } else throw new UserInputError('Post not found')
+                } else throw new UserInputError('Post not found')  
+            }
+        },
+        async deleteComment(_:any, {postId, commentId}: IDeleteCom, context: any){
+            const {username}  = checkAuth(context)
+            const post = await Post.findById(postId)
+
+            if(post){
+                const commentIndex = post.comments.findIndex((c:Comments) => c.id === commentId)
+
+                if(post.comments[commentIndex].username === username){
+                    post.comments.splice(commentIndex, 1)
+                    await post.save()
+                    return post
+                } else{
+                    throw new AuthenticationError('Action not allowed')
+                }
+            } else{
+                throw new UserInputError('Post not found')
             }
         }
     }
